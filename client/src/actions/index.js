e520d8f6bdc79db977ce92e38c11f8e30c44641e
Felipe Guitelmane <felipe.guitelman@.com>
2022-06-14 22:38:32 -0300

    deployment

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export function getPokemons() {
   return async function (dispatch) {
-    var json = await axios.get("http://localhost:3001/pokemons", {});
+    var json = await axios.get("/pokemons", {});
     return dispatch({
       type: 'GET_POKEMONS',
       payload: json.data
@@ -13,7 +13,7 @@ export function getPokemons() {
 export function getNamePokemon(name) {
   return async function (dispatch) {
     try {
-      var json = await axios.get("http://localhost:3001/pokemons/name?name="+ name);
+      var json = await axios.get("/pokemons/name?name="+ name);
       return dispatch({
         type: 'GET_NAME_POKEMON',
         payload: json.data
@@ -31,7 +31,7 @@ export function getNamePokemon(name) {
 
 export function getAllTypes() {
   return async function (dispatch) {
-    var json = await axios.get("http://localhost:3001/types/", {});
+    var json = await axios.get("/types/", {});
     return dispatch({
       type: 'GET_TYPES',
       payload: json.data
@@ -41,7 +41,7 @@ export function getAllTypes() {
 
 export function postPokemon(payload){
   return async function (dispatch){
-    const response = await axios.post("http://localhost:3001/pokemons/", payload);
+    const response = await axios.post("/pokemons/", payload);
     return response
   }
 }
@@ -84,7 +84,7 @@ export function orderByLife(payload) {
 export function getDetail (id){
   return async function (dispatch) {
     try {
-      var json = await axios.get("http://localhost:3001/pokemons/"+ id);
+      var json = await axios.get("/pokemons/"+ id);
       return dispatch({
         type: "GET_DETAIL",
         payload: json.data

5d65acb783fbc42a66974ceb2c15e95b8aeb6536
Felipe Guitelmane <felipe.guitelman@.com>
2022-08-18 11:07:15 -0300

    nuevo deploy final

--- client/src/components/Landing.jsx
+++ client/src/components/Landing.jsx
@@ -5,7 +5,7 @@ import { Button } from './Detail'
 export default function LandingPage(){
   return(
     <div>
-      <h1 style={{color:'#cd5c5c'}}>Welcome to my PokeApi!!</h1>
+      <h1 style={{color:'#cd5c5c'}}>Welcome to my PokeApi!</h1>
       <Link to = '/home'>
         <Button>Start</Button>
       </Link>
